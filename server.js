e39e60940ac20b0feacb6193f3d72103f96cdfd2
Lalit Deore <lalit.deore@gmail.com>
2019-09-08 15:54:16 +0530

    UI & Alerts redux store

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
+connectDB();
+
+// Init Middleware
+// We can skip installation of bodyparser.json
+app.use(express.json({exptended:false}));
+
 app.get('/', (req, res)=> res.send('API Running'));
+
+// Define Routes
+require('./')
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
